0bedae69177c24847a2b42ab87bcb81aeed12ef5
jgonext <jgo.next@gmail.com>
2020-10-06 21:15:49 +0200

    Integración web view

--- app/src/main/java/es/pue/intentpractice/presentationlayer/controllers/activities/GpsActivity.kt
+++ app/src/main/java/es/pue/intentpractice/presentationlayer/controllers/activities/GpsActivity.kt
@@ -1,12 +1,30 @@
 package es.pue.intentpractice.presentationlayer.controllers.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.location.Geocoder
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.intentpractice.R
+import kotlinx.android.synthetic.main.activity_gps.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_web.*
+import java.net.URLEncoder
+
 
 class GpsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gps)
+
+        ag_btn_find.setOnClickListener(){
+            val query: String = Uri.encode(ag_txt.text.toString())
+            val uri = Uri.parse("geo:41.387295, 2.190003?q=$query")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            //intent.setPackage("com.google.android.apps.maps")
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/es/pue/intentpractice/presentationlayer/controllers/activities/WebActivity.kt
+++ app/src/main/java/es/pue/intentpractice/presentationlayer/controllers/activities/WebActivity.kt
@@ -1,13 +1,37 @@
 package es.pue.intentpractice.presentationlayer.controllers.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.intentpractice.R
+import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+
+        //val escapedQuery: String = URLEncoder.encode(aw_txt.text.toString(), "UTF-8")
+        //val uri: Uri = Uri.parse("https://www.google.com/search?q=$escapedQuery")
+
+//        val start: (String) -> Unit = { pck ->
+//            val uri: Uri = Uri.parse("http://" + aw_txt.text.toString())
+//            val intent = Intent(Intent.ACTION_VIEW, uri)
+//            intent.setPackage(pck)
+//            startActivity(intent)
+//        }
+//        aw_btn_browser.setOnClickListener(){ start("com.android.chrome") }
+//        aw_btn_search.setOnClickListener(){ start("org.chromium.webview_shell") }
+
+        aw_btn_browser.setOnClickListener(){
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://" + aw_txt.text.toString()))
+            startActivity(intent)
+        }
+        
+
+
+
     }
 }
 
